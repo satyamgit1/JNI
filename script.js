10f0629311370d878dfed3e9621cf153f41a3604
Satyam singh <satyamsingh2003a@gmail.com>
2024-07-11 16:21:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// let items = document.querySelectorAll('.carousel-item');
+// let currentItem = 0;
+// // let interval = setInterval(nextItem, 1000);
+
+// function nextItem() {
+//     console.log('Next item function called');
+//     items[currentItem].classList.remove('active');
+//     currentItem = (currentItem + 1) % items.length;
+//     items[currentItem].classList.add('active');
+// }
+
+// function prevItem() {
+//     console.log('Prev item function called');
+//     items[currentItem].classList.remove('active');
+//     currentItem = (currentItem - 1 + items.length) % items.length;
+//     items[currentItem].classList.add('active');
+// }
+
+// document.querySelector('.carousel-next').addEventListener('click', function(e) {
+//     e.preventDefault();
+//     console.log('Next button clicked');
+//     clearInterval(interval);
+//     nextItem();
+// });
+
+// document.querySelector('.carousel-prev').addEventListener('click', function(e) {
+//     e.preventDefault();
+//     console.log('Prev button clicked');
+//     clearInterval(interval);
+//     prevItem();
+// });
+
+
